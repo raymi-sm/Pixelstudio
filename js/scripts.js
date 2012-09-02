d199bc1d16b31b8662eae9f504b5df10f062956f
Raymi Saldomando <raymi15@gmail.com>
2012-09-02 19:28:48 -0400

    agregando codigo de trabajo para portafolio

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {	
+		    $('#slider').cycle({
+				fx: 'fade', // choose your transition type, ex: fade, scrollUp, shuffle, etc...
+				next: '#next',
+				prev: '#prev',
+				pager: '#numericNav'
+			});
+		});
